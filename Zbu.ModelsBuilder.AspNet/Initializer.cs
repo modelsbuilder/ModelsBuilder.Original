acfd8f52d342da8a8a4717791e63addbb33b4a65
Stephan <sgay@pilotine.com>
2013-10-30 18:00:14 +0100

    Add ASP.NET integration project (wip)

---
+++ Zbu.ModelsBuilder.AspNet/Initializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Compilation;
+
+[assembly: PreApplicationStartMethod(typeof(Zbu.ModelsBuilder.AspNet.Initializer), "Initialize")]
+
+namespace Zbu.ModelsBuilder.AspNet
+{
+    public static class Initializer
+    {
+        public static void Initialize()
+        {
+            BuildProvider.RegisterBuildProvider(".models", typeof(ModelsBuildProvider));
+            BuildManager.AddReferencedAssembly(typeof(IgnoreContentTypeAttribute).Assembly);
+        }
+    }
+}
