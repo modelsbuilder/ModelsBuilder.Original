a295baedaf1fb70a5388e535f12557510ced2f2b
Stephan <sgay@pilotine.com>
2013-10-30 10:22:17 +0100

    Options now properly saved to a project-local, XML file

--- Zbu.ModelsBuilder.CustomTool/VisualStudioOptions.cs
+++ Zbu.ModelsBuilder.CustomTool/VisualStudioOptions.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.VisualStudio.Shell;
 using Zbu.ModelsBuilder.CustomTool.VisualStudio;
 
@@ -34,39 +37,77 @@ namespace Zbu.ModelsBuilder.CustomTool
         // by default "storage" is the registry
         // we want to write to our own settings file,
         // <solution>.sln.zbu.user
-        // <zbu>
-        //   <modelsBuilder version="1.4.0.0" connectionString="..." databaseProvider="..." />
-        // </zbu>
+        // and yes - we should prob. use true config sections,
+        // not parse XML...
 
         public override void LoadSettingsFromStorage()
         {
             //base.LoadSettingsFromStorage();
 
-            var sln = VisualStudioHelper.GetSolution();
-            var cfg = sln + ".zbu.user";
-            if (File.Exists(cfg))
-            {
-                var txt = File.ReadAllText(cfg);
-                var pos = txt.IndexOf("|||");
-                ConnectionString = txt.Substring(0, pos);
-                DatabaseProvider = txt.Substring(pos + 3);
-            }
+            var solution = VisualStudioHelper.GetSolution();
+            var filename = solution + ".zbu.user";
+            if (!File.Exists(filename)) return;
+
+            var text = File.ReadAllText(filename);
+            var xml = new XmlDocument();
+            xml.LoadXml(text);
+
+            var config = xml.SelectSingleNode("/configuration/zbu/modelsBuilder");
+            if (config == null || config.Attributes == null) return;
+
+            var attr = config.Attributes["version"];
+            if (attr == null) return;
+            var version = attr.Value;
+
+            // we're not version-dependent at the moment
+            attr = config.Attributes["connectionString"];
+            if (attr != null)
+                ConnectionString = attr.Value;
+            attr = config.Attributes["databaseProvider"];
+            if (attr != null)
+                DatabaseProvider = attr.Value;
         }
 
         public override void SaveSettingsToStorage()
         {
             //base.SaveSettingsToStorage();
 
-            // fixme - just testing, we need a better format
-            // fixme - must also serialize OUR own version for upgrade purpose
-            var sln = VisualStudioHelper.GetSolution();
-            var cfg = sln + ".zbu.user";
-            if (File.Exists(cfg))
-                File.Delete(cfg);
-            File.WriteAllText(cfg, ConnectionString + "|||" + DatabaseProvider);
+            var solution = VisualStudioHelper.GetSolution();
+            var filename = solution + ".zbu.user";
+
+            if (File.Exists(filename))
+                File.Delete(filename);
+
+            var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            var sb = new StringBuilder();
+            var settings = new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                OmitXmlDeclaration = true, // 'cos it's utf-16 and a pain to change
+                Indent = true,
+                NewLineChars = "\r\n"
+            };
+            var writer = XmlWriter.Create(sb, settings);
+
+            sb.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+
+            writer.WriteStartElement("configuration");
+            writer.WriteStartElement("zbu");
+            writer.WriteStartElement("modelsBuilder");
+            writer.WriteAttributeString("version", version);
+            writer.WriteAttributeString("connectionString", ConnectionString);
+            writer.WriteAttributeString("databaseProvider", DatabaseProvider);
+            writer.WriteEndElement(); // modelsBuilder
+            writer.WriteEndElement(); // zbu
+            writer.WriteEndElement(); // configuration
+            writer.Flush();
+            writer.Close();
+
+            File.WriteAllText(filename, sb.ToString());
         }
 
-        // FIXME but what about the FromXml / ToXml methods?!
+        // what about the FromXml / ToXml methods?!
         // that would be for import/export?!
     }
 }
