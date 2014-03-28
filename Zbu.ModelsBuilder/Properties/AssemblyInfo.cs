e5b5cbb369d4e8b909fdf02b5e537696d195140c
Stephan <sgay@pilotine.com>
2014-03-28 09:36:22 +0100

    fold

--- Zbu.ModelsBuilder/Properties/AssemblyInfo.cs
+++ Zbu.ModelsBuilder/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ZpqrtBnk Umbraco ModelsBuilder")]
 [assembly: AssemblyDescription("ZpqrtBnk Umbraco tool for generating strongly typed IPublishedContent models.")]
 [assembly: Guid("b59ebab2-bc7c-4a89-876f-7613684510e2")]
+
+[assembly: InternalsVisibleTo("Zbu.ModelsBuilder.Tests")]
