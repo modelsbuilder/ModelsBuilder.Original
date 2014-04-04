62f9edf87b8675da8ef350e45a800bb2bb44d680
Stephan <stephane@umbraco.dk>
2014-04-04 12:33:45 +0200

    Bump version to 1.5.O

--- Zbu.ModelsBuilder/Properties/CommonInfo.cs
+++ Zbu.ModelsBuilder/Properties/CommonInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("ZpqrtBnk Umbraco ModelsBuilder")]
 [assembly: AssemblyCompany("Pilotine - ZpqrtBnk")]
-[assembly: AssemblyCopyright("Copyright © Pilotine - ZpqrtBnk 2013")]
+[assembly: AssemblyCopyright("Copyright © Pilotine - ZpqrtBnk 2013-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
