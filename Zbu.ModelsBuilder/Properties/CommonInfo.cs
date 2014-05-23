e7cdbb5da4ad37699fe7c63a10412d756228bfcb
Stephan <stephane@umbraco.dk>
2014-05-23 20:34:52 +0200

    Bump version to 1.6.0.1

--- Zbu.ModelsBuilder/Properties/CommonInfo.cs
+++ Zbu.ModelsBuilder/Properties/CommonInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.5.0.4")]
-[assembly: AssemblyFileVersion("1.5.0.4")]
+[assembly: AssemblyVersion("1.6.0.1")]
+[assembly: AssemblyFileVersion("1.6.0.1")]
