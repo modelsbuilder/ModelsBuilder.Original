2c0ca188ea906427e2d3e3f77df80d3fc12c393f
Stephan <stephane@umbraco.dk>
2014-06-08 16:28:16 +0200

    Version 1.7.0.0

--- Zbu.ModelsBuilder/Properties/CommonInfo.cs
+++ Zbu.ModelsBuilder/Properties/CommonInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.6.0.1")]
-[assembly: AssemblyFileVersion("1.6.0.1")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
