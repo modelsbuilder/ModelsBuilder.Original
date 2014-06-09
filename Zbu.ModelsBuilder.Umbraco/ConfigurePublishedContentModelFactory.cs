05ce2b58b9749729950a20774aeeefa056d5b366
Stephan <stephane@umbraco.dk>
2014-06-09 12:41:38 +0200

    Configure ModelsFactory by default, can disable via appSettings

---
+++ Zbu.ModelsBuilder.Umbraco/ConfigurePublishedContentModelFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Zbu.ModelsBuilder.Umbraco
+{
+    public class ConfigurePublishedContentModelFactory : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            if (ConfigurationManager.AppSettings["Zbu.ModelsBuilder.AspNet.ConfigureFactoryResolver"] == "false") return;
+
+            var types = PluginManager.Current.ResolveTypes<PublishedContentModel>();
+            var factory = new PublishedContentModelFactory(types);
+            PublishedContentModelFactoryResolver.Current.SetFactory(factory);
+        }
+    }
+}
