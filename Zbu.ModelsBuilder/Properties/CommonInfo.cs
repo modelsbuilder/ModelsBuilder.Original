ce2a52cdce520735599337ffdaca2e1a09d726a4
Stephan <stephane@umbraco.dk>
2014-06-20 18:49:21 +0200

    Version 2.0.0-beta001

--- Zbu.ModelsBuilder/Properties/CommonInfo.cs
+++ Zbu.ModelsBuilder/Properties/CommonInfo.cs
@@ -19,11 +19,18 @@ using System.Runtime.InteropServices;
 // 1.8.0-alpha001   1.8.0.0
 // 1.8.0-alpha002   1.8.0.1
 // 1.8.0-alpha      1.8.0.2
-// 1.8.0            1.8.0.3
+// 1.8.0-beta001    1.8.0.3
+// 1.8.0-beta002    1.8.0.4
+// 1.8.0-beta       1.8.0.5
+// 1.8.0            1.8.0.6
+
+// Vsix
+// Also need to 
 
 // Assembly
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.6")]
+[assembly: AssemblyFileVersion("2.0.0.6")]
 
 // NuGet Package
-[assembly: AssemblyInformationalVersion("2.0.0-alpha000")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta001")]
+// Do not remove this line.
