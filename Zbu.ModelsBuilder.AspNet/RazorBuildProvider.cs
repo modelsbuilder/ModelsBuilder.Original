0e02e89ba736207b23dc75ada5992b0832dd2445
Stephan <stephane@umbraco.dk>
2014-07-04 15:30:07 +0200

    Unplug pure live models [#6]

--- Zbu.ModelsBuilder.AspNet/RazorBuildProvider.cs
+++ Zbu.ModelsBuilder.AspNet/RazorBuildProvider.cs
@@ -22,101 +22,24 @@ namespace Zbu.ModelsBuilder.AspNet
     // This build provider is NOT installed in web.config as shown above
     // It is installed at runtime by the Initializer class, if required (depends on config)
 
+    // NOTE
+    // this is not used anymore because, to some extent, it cannot work
+    // see note in ConfigurePublishedContentModelFactory.cs
+
     [BuildProviderAppliesTo(BuildProviderAppliesTo.Web | BuildProviderAppliesTo.Code)]
     public class RazorBuildProvider : System.Web.WebPages.Razor.RazorBuildProvider
     {
-        private static readonly object LockO = new object();
-        private static bool _triedToGetModelsAssemblyAlready;
-        private static bool _initialized;
-        private static Assembly _modelsAssembly;
-
         public override void GenerateCode(AssemblyBuilder assemblyBuilder)
         {
             // if pure live models are enabled, compile & add assembly
             if (Config.EnablePureLiveModels)
-                AddModelsAssemblyReference(assemblyBuilder);
+            {
+                var modelsAssembly = ModelsAssemblyProvider.ModelsAssembly;
+                if (modelsAssembly != null)
+                    assemblyBuilder.AddAssemblyReference(modelsAssembly);
+            }
 
             base.GenerateCode(assemblyBuilder);
-        }
-
-        private static void Initialize()
-        {
-            // anything changes, and we want to re-generate models.
-            ContentTypeCacheRefresher.CacheUpdated += (sender, args) => ResetModelsAssembly();
-            DataTypeCacheRefresher.CacheUpdated += (sender, args) => ResetModelsAssembly();
-
-            _initialized = true;
-        }
-
-        private static void ResetModelsAssembly()
-        {
-            lock (LockO)
-            {
-                _modelsAssembly = null;
-                _triedToGetModelsAssemblyAlready = false;
-            }
-        }
-
-        private static void AddModelsAssemblyReference(AssemblyBuilder assemblyBuilder)
-        {
-            lock (LockO)
-            {
-                if (!_initialized)
-                    Initialize();
-
-                if (_modelsAssembly == null && !_triedToGetModelsAssemblyAlready)
-                {
-                    _modelsAssembly = GetModelsAssembly();
-                    _triedToGetModelsAssemblyAlready = true;
-                }
-            }
-            if (_modelsAssembly != null)
-                assemblyBuilder.AddAssemblyReference(_modelsAssembly);
-        }
-
-        private static Assembly GetModelsAssembly()
-        {
-            // ensure we have a proper App_Data directory
-            var appData = HostingEnvironment.MapPath("~/App_Data");
-            if (appData == null || !Directory.Exists(appData)) return null;
-
-            var umbraco = Application.GetApplication();
-            var typeModels = umbraco.GetAllTypes();
-
-            var builder = new TextBuilder(typeModels, ParseResult.Empty, Config.ModelsNamespace);
-
-            var code = new StringBuilder();
-            builder.Generate(code, builder.GetModelsToGenerate());
-
-            // use the Roslyn compiler!
-            var compiler = new Compiler();
-            foreach (var asm in BuildManager.GetReferencedAssemblies().Cast<Assembly>())
-                compiler.ReferencedAssemblies.Add(asm);
-            return compiler.Compile(builder.GetModelsNamespace(), code.ToString());
-
-            // use the BuildManager compiler...
-            /*
-            // write the code to a temp file
-            // cannot be in Path.GetTempPath() because GetCompiledAssembly wants code in the website
-            //var temp = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".cs");
-            //var virttemp = "~/App_Data/Models/tmp." + Guid.NewGuid() + ".cs";
-            var virttemp = "~/App_Data/tmp." + Guid.NewGuid() + ".cs";
-            var temp = HostingEnvironment.MapPath(virttemp);
-            if (temp == null)
-                throw new Exception("Failed to map temp file.");
-            File.WriteAllText(temp, code.ToString());
-
-            try
-            {
-                // get the compiled assembly
-                return BuildManager.GetCompiledAssembly(virttemp);
-            }
-            finally
-            {
-                // make sure whatever happens we properly delete the temp file
-                File.Delete(temp);
-            }
-            */
         }
     }
 }
--- Zbu.ModelsBuilder.Umbraco/ConfigurePublishedContentModelFactory.cs
+++ Zbu.ModelsBuilder.Umbraco/ConfigurePublishedContentModelFactory.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Core;
+﻿using System;
+using Umbraco.Core;
 using Umbraco.Core.Models.PublishedContent;
 using Zbu.ModelsBuilder.Configuration;
 
@@ -10,6 +11,19 @@ namespace Zbu.ModelsBuilder.Umbraco
         {
             if (!Config.EnablePublishedContentModelsFactory)
                 return;
+
+            // NOTE
+            // this is done once when the app starts, which means that "pure" live models
+            // are not being detected (not compiled yet) and cannot be updated once they
+            // have been re-generated. We cannot compile then right now, because we don't
+            // have access to the DB etc yet. We cannot register them later on to the
+            // factory (provided that we modify the Umbraco factory to support it) because
+            // then... the factory will switch to one version of type "Foo" to another version
+            // of the same time right in the middle of a request... bad. And the factory is
+            // bound to the cache, ie it's global -- not one per request, otherwise we
+            // lose the perfs benefit.
+            //
+            // conclusion... RIP pure live models
 
             var types = PluginManager.Current.ResolveTypes<PublishedContentModel>();
             var factory = new PublishedContentModelFactory(types);
