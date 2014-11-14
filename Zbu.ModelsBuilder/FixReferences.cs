9416efd14fccda522568bff65bfbf9de7aab7b2e
Stephan <stephane@umbraco.dk>
2014-11-14 11:08:30 +0100

    Fix missing dependencies in Umbraco package [#23]

---
+++ Zbu.ModelsBuilder/FixReferences.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Zbu.ModelsBuilder
+{
+    static class FixReferences
+    {
+        static FixReferences()
+        {
+            // DO NOT DELETE
+            // references Microsoft.CodeAnalysis.CSharp.Desktop assembly
+            // so that it is included in dependent projects (otherwise it's not)
+            // read http://stackoverflow.com/questions/1132243/msbuild-doesnt-copy-references-dlls-if-using-project-dependencies-in-solution
+            var parser = CSharpCommandLineParser.Default;
+            parser.Parse(Enumerable.Empty<string>(), string.Empty);
+        }
+    }
+}
