d612320da234633b12f63aa831238523707f3088
Stephan <stephane@umbraco.dk>
2015-01-28 16:00:23 +0100

    System.Version as string in WebApi [#34]

--- Zbu.ModelsBuilder.AspNet/ModelsBuilderApiController.cs
+++ Zbu.ModelsBuilder.AspNet/ModelsBuilderApiController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Web.Compilation;
 using System.Web.Hosting;
@@ -38,17 +39,58 @@ namespace Zbu.ModelsBuilder.AspNet
             public string Message;
         }
 
+        [DataContract]
         public class ValidateClientVersionData
         {
+            // issues 32, 34... problems when serializing versions
+            //
+            // make sure System.Version objects are transfered as strings
+            // depending on the JSON serializer version, it looks like versions are causing issues
+            // see
+            // http://stackoverflow.com/questions/13170386/why-system-version-in-json-string-does-not-deserialize-correctly
+            //
+            // if the class is marked with [DataContract] then only properties marked with [DataMember]
+            // are serialized and the rest is ignored, see
+            // http://www.asp.net/web-api/overview/formats-and-model-binding/json-and-xml-serialization
+
+            [DataMember]
+            public string ClientVersionString
+            {
+                get { return ClientVersion.ToString(); }
+                set { ClientVersion = ParseVersion(value, false, "client"); }
+            }
+
+            [DataMember]
+            public string MinServerVersionSupportingClientString
+            {
+                get { return MinServerVersionSupportingClient.ToString(); }
+                set { MinServerVersionSupportingClient = ParseVersion(value, true, "minServer"); }
+            }
+
+            // not serialized
             public Version ClientVersion { get; set; }
             public Version MinServerVersionSupportingClient { get; set; }
+
+            private static Version ParseVersion(string value, bool canBeNull, string name)
+            {
+                if (string.IsNullOrWhiteSpace(value) && canBeNull)
+                    return null;
+
+                Version version;
+                if (Version.TryParse(value, out version))
+                    return version;
+
+                throw new ArgumentException(string.Format("Failed to parse \"{0}\" as {1} version.", value, name));
+            }
         }
 
-        public class GetModelsData
+        [DataContract]
+        public class GetModelsData : ValidateClientVersionData
         {
-            public Version ClientVersion { get; set; }
-            public Version MinServerVersionSupportingClient { get; set; }
+            [DataMember]
             public string Namespace { get; set; }
+
+            [DataMember]
             public IDictionary<string, string> Files { get; set; }
         }
 
