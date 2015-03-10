633f10860e05605e46909759a1db655361f4669f
Stephan <stephane@umbraco.dk>
2015-03-10 18:04:20 +0100

    Version 2.1.0-final

--- Zbu.ModelsBuilder/Properties/CommonInfo.cs
+++ Zbu.ModelsBuilder/Properties/CommonInfo.cs
@@ -28,11 +28,11 @@ using System.Runtime.InteropServices;
 // Also need to 
 
 // Assembly
-[assembly: AssemblyVersion("2.1.0.36")]
-[assembly: AssemblyFileVersion("2.1.0.36")]
+[assembly: AssemblyVersion("2.1.0.37")]
+[assembly: AssemblyFileVersion("2.1.0.37")]
 
 // NuGet Package
 // Note: cannot release "1.8.0" because it depends on pre-release NuGet packages
 // so I have to use 1.8.0-final...
-[assembly: AssemblyInformationalVersion("2.1.0-beta001")]
+[assembly: AssemblyInformationalVersion("2.1.0-final")]
 // Do not remove this line.
