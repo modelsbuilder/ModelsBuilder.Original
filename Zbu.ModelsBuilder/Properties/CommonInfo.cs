c5bc56ade78567347b7213efc0d8e4230cf8e62b
Stephan <stephane@umbraco.dk>
2015-05-03 16:32:18 +0200

    Update copyright notice

--- Zbu.ModelsBuilder/Properties/CommonInfo.cs
+++ Zbu.ModelsBuilder/Properties/CommonInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("ZpqrtBnk Umbraco ModelsBuilder")]
 [assembly: AssemblyCompany("Pilotine - ZpqrtBnk")]
-[assembly: AssemblyCopyright("Copyright © Pilotine - ZpqrtBnk 2013-2014")]
+[assembly: AssemblyCopyright("Copyright © Pilotine - ZpqrtBnk 2013-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
