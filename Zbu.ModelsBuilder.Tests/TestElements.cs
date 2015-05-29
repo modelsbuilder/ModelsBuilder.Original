39613bcbe11ce2d7d8621841e874d95446bc14b4
Stephan <stephane@umbraco.dk>
2015-05-29 10:15:47 +0200

    Unit testing WIP

---
+++ Zbu.ModelsBuilder.Tests/TestElements.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Zbu.ModelsBuilder.Tests
+{
+    class TestElements
+    {
+        public class PublishedProperty : IPublishedProperty
+        {
+            private readonly string _alias;
+            private readonly object _dataValue;
+
+            public PublishedProperty(string alias, object dataValue)
+            {
+                _alias = alias;
+                _dataValue = dataValue;
+            }
+
+            public object DataValue
+            {
+                get { return _dataValue; }
+            }
+
+            public bool HasValue
+            {
+                get { return _dataValue != null; }
+            }
+
+            public string PropertyTypeAlias
+            {
+                get { return _alias; }
+            }
+
+            public object Value
+            {
+                get { return _dataValue; }
+            }
+
+            public object XPathValue
+            {
+                get { throw new NotImplementedException(); }
+            }
+        }
+
+        public class PublishedContent : IPublishedContent
+        {
+            private readonly PublishedContentType _contentType;
+            private readonly PublishedProperty[] _properties;
+
+            public PublishedContent(PublishedContentType contentType, PublishedProperty[] properties)
+            {
+                _contentType = contentType;
+                _properties = properties;
+            }
+
+            public IEnumerable<IPublishedContent> Children
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public IEnumerable<IPublishedContent> ContentSet
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public PublishedContentType ContentType
+            {
+                get { return _contentType; }
+            }
+
+            public DateTime CreateDate
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public int CreatorId
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string CreatorName
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string DocumentTypeAlias
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public int DocumentTypeId
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public int GetIndex()
+            {
+                throw new NotImplementedException();
+            }
+
+            public IPublishedProperty GetProperty(string alias, bool recurse)
+            {
+                return GetProperty(alias);
+            }
+
+            public IPublishedProperty GetProperty(string alias)
+            {
+                return Properties.FirstOrDefault(x => x.PropertyTypeAlias.InvariantEquals(alias));
+            }
+
+            public int Id
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public bool IsDraft
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public PublishedItemType ItemType
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public int Level
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string Name
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public IPublishedContent Parent
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string Path
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public ICollection<IPublishedProperty> Properties
+            {
+                get { return _properties; }
+            }
+
+            public int SortOrder
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public int TemplateId
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public DateTime UpdateDate
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string Url
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string UrlName
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public Guid Version
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public int WriterId
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string WriterName
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public object this[string alias]
+            {
+                get { throw new NotImplementedException(); }
+            }
+        }
+    }
+}
---
+++ Zbu.ModelsBuilder.Tests/UmbracoInternals.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Zbu.ModelsBuilder.Tests
+{
+    class UmbracoInternals
+    {
+        private static T Ctor<T>(params object[] args)
+        {
+            var types = args.Select(x => x.GetType()).ToArray();
+            var ctor = typeof(T).GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, types, null);
+            return (T)ctor.Invoke(args);
+        }
+
+        public static void InitializeConverters()
+        {
+            var r = Ctor<PropertyValueConvertersResolver>((IEnumerable<Type>)new Type[0]);
+            PropertyValueConvertersResolver.Current = r;
+        }
+
+        public static void FreezeResolution()
+        {
+            var t = typeof(ApplicationContext)
+                .Assembly
+                .GetType("Umbraco.Core.ObjectResolution.Resolution");
+            var m = t.GetMethod("Freeze");
+            m.Invoke(null, new object[0]);
+        }
+
+        public static PublishedPropertyType CreatePublishedPropertyType(string alias, int definition, string editor)
+        {
+            return Ctor<PublishedPropertyType>(alias, definition, editor);
+        }
+
+        public static PublishedContentType CreatePublishedContentType(int id, string alias, IEnumerable<PublishedPropertyType> propertyTypes)
+        {
+            return Ctor<PublishedContentType>(id, alias, propertyTypes);
+        }
+    }
+}
