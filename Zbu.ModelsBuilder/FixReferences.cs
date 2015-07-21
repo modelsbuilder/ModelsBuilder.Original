94b9e2cf9c7d875aae22855dddcda66ee72ea3b9
Stephan <stephane@umbraco.dk>
2015-07-21 08:57:12 +0200

    Upgrade Roslyn to 1.0.0

--- Zbu.ModelsBuilder/FixReferences.cs
+++ Zbu.ModelsBuilder/FixReferences.cs
@@ -12,7 +12,7 @@ namespace Zbu.ModelsBuilder
             // so that it is included in dependent projects (otherwise it's not)
             // read http://stackoverflow.com/questions/1132243/msbuild-doesnt-copy-references-dlls-if-using-project-dependencies-in-solution
             var parser = CSharpCommandLineParser.Default;
-            parser.Parse(Enumerable.Empty<string>(), string.Empty);
+            parser.Parse(Enumerable.Empty<string>(), string.Empty, string.Empty);
         }
     }
 }
