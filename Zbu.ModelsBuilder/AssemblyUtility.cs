1cfb31f24ee6cf58c316519c8554384e1f22a0ec
Stephan <stephane@umbraco.dk>
2015-09-23 16:45:09 +0200

    Prevent Roslyn from loading dynamic asm [#68]

--- Zbu.ModelsBuilder/AssemblyUtility.cs
+++ Zbu.ModelsBuilder/AssemblyUtility.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Zbu.ModelsBuilder
 {
+    // issue [#67]
+    // GetAllReferencedAssemblyLocations throws on dynamic assemblies that would
+    // be loaded in the current AppDomain, because these assemblies do not have
+    // a location.
+    // We can either fix it by either ignoring dynamic assemblies, or finding a
+    // way to create a MetadataReference to an assembly that exists only in
+    // memory.
+    // Cannot find a way to create such a MetadataReference as it can only create
+    // from the assembly's bytes, which we don't have. However, if the assembly
+    // exists only in memory, it cannot really be referenced in a compilation,
+    // so it should be fine to exclude it.
+    // Fixing by adding .Where(x => x.IsDynamic == false)
+
     public static class AssemblyUtility
     {
         // fixme - this is slow and should probably be cached in a static var!
@@ -17,7 +28,7 @@ namespace Zbu.ModelsBuilder
             var assemblies = new List<Assembly>();
             var tmp1 = new List<Assembly>();
             var failed = new List<AssemblyName>();
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies().Where(x => x.IsDynamic == false))
             {
                 assemblies.Add(assembly);
                 tmp1.Add(assembly);
