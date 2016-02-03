eea777a89ed20c370b8428275d32fcf4eac66fef
Shannon <sdeminick@gmail.com>
2016-02-03 11:46:16 +0100

    removes the old ASPNet files laying around

--- Umbraco.ModelsBuilder.AspNet/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Umbraco ModelsBuilder Site Integration")]
-[assembly: AssemblyDescription("Umbraco ModelsBuilder Site Integration.")]
-[assembly: Guid("f1da67b7-13dc-4fa4-bec2-747d7bdb93cf")]
