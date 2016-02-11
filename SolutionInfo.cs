7d9f6c310fa81224c2cd41bff31f07a6a8a8a108
Shannon <sdeminick@gmail.com>
2016-02-11 16:22:54 +0100

    bumps version

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -28,12 +28,12 @@ using System.Runtime.InteropServices;
 // Also need to
 
 // Assembly
-[assembly: AssemblyVersion("3.0.0.81")]
-[assembly: AssemblyFileVersion("3.0.0.81")]
+[assembly: AssemblyVersion("3.0.0.90")]
+[assembly: AssemblyFileVersion("3.0.0.90")]
 
 // NuGet Package
 // Note: could not release "1.8.0" because it was depending on pre-release NuGet packages
 //  for Roslyn, so had to release 1.8.0-final... starting with 2.1.3 Roslyn has a released
 //  1.0 version, so now we can release "2.1.3" without the "-final" extension.
-[assembly: AssemblyInformationalVersion("3.0.0.81-rc2")]
+[assembly: AssemblyInformationalVersion("3.0.0.90")]
 // Do not remove this line.
