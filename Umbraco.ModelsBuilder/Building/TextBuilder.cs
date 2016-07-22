6eaff42286851fb4f1fe158990c51f893451c144
Jeremy Pyne <jpyne@proteusb2b.com>
2016-07-22 14:52:10 -0400

    Added strong typed GetPropertyValue methods to model generation code.

--- Umbraco.ModelsBuilder/Building/TextBuilder.cs
+++ Umbraco.ModelsBuilder/Building/TextBuilder.cs
@@ -197,6 +197,23 @@ namespace Umbraco.ModelsBuilder.Building
                 type.ClrName);
             sb.Append("\t\t{\n\t\t\treturn PublishedContentModelUtility.GetModelPropertyType(GetModelContentType(), selector);\n\t\t}\n");
 
+            // write out strong type GetPropertyValue methods
+            sb.AppendFormat("\t\tpublic TValue GetPropertyValue<TValue>(Expression<Func<{0}, TValue>> selector)\n",
+                type.ClrName);
+            sb.Append("\t\t{\n\t\t\treturn this.GetPropertyValue<TValue>(PublishedContentModelUtility.GetModelPropertyAlias(selector));\n\t\t}\n");
+
+            sb.AppendFormat("\t\tpublic TValue GetPropertyValue<TValue>(Expression<Func<{0}, TValue>> selector, bool recurse)\n",
+                type.ClrName);
+            sb.Append("\t\t{\n\t\t\treturn this.GetPropertyValue<TValue>(PublishedContentModelUtility.GetModelPropertyAlias(selector), recurse);\n\t\t}\n");
+
+            sb.AppendFormat("\t\tpublic TValue GetPropertyValue<TValue>(Expression<Func<{0}, TValue>> selector, TValue defaultValue)\n",
+                type.ClrName);
+            sb.Append("\t\t{\n\t\t\treturn this.GetPropertyValue<TValue>(PublishedContentModelUtility.GetModelPropertyAlias(selector), defaultValue);\n\t\t}\n");
+
+            sb.AppendFormat("\t\tpublic TValue GetPropertyValue<TValue>(Expression<Func<{0}, TValue>> selector, bool recurse, TValue defaultValue)\n",
+                type.ClrName);
+            sb.Append("\t\t{\n\t\t\treturn this.GetPropertyValue<TValue>(PublishedContentModelUtility.GetModelPropertyAlias(selector), recurse, defaultValue);\n\t\t}\n");
+
             // write the properties
             WriteContentTypeProperties(sb, type);
 
