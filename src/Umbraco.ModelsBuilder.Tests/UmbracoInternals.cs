596f2309c3c45f488e987abca77dd03ee990d63f
Stephan <stephane@umbraco.dk>
2018-08-28 19:41:53 +0200

    On Umbraco -alpha.49, version -alpha.22

--- src/Umbraco.ModelsBuilder.Tests/UmbracoInternals.cs
+++ src/Umbraco.ModelsBuilder.Tests/UmbracoInternals.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using LightInject;
 using Moq;
-using Umbraco.Core;
-using Umbraco.Core.Logging;
 using Umbraco.Core.Models;
 using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Core.PropertyEditors;
-using Umbraco.Web.Composing;
 
 namespace Umbraco.ModelsBuilder.Tests
 {
@@ -20,14 +14,12 @@ namespace Umbraco.ModelsBuilder.Tests
             var valueConverters = new PropertyValueConverterCollection(Enumerable.Empty<IPropertyValueConverter>());
             var publishedModelFactory = Mock.Of<IPublishedModelFactory>();
             var publishedContentTypeFactory = Mock.Of<IPublishedContentTypeFactory>();
-            return new PublishedPropertyType(alias, definition, false, ContentVariation.InvariantNeutral, valueConverters, publishedModelFactory, publishedContentTypeFactory);
-            //return Ctor<PublishedPropertyType>(alias, definition, editor, false);
+            return new PublishedPropertyType(alias, definition, false, ContentVariation.Nothing, valueConverters, publishedModelFactory, publishedContentTypeFactory);
         }
 
         public static PublishedContentType CreatePublishedContentType(int id, string alias, IEnumerable<PublishedPropertyType> propertyTypes)
         {
-            return new PublishedContentType(id, alias, PublishedItemType.Content, Enumerable.Empty<string>(), propertyTypes, ContentVariation.InvariantNeutral);
-            //return Ctor<PublishedContentType>(id, alias, propertyTypes);
+            return new PublishedContentType(id, alias, PublishedItemType.Content, Enumerable.Empty<string>(), propertyTypes, ContentVariation.Nothing);
         }
     }
 }
